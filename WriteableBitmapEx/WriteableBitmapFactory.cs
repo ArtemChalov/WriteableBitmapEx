14551ea61a83263ad914e19039100dede53baa8a
ArtemChalov <chalov81@mail.ru>
2018-12-04 23:24:58 +0500

    Добавьте файлы проекта.

---
+++ WriteableBitmapEx/WriteableBitmapFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WriteableBitmapEx
+{
+    public class WriteableBitmapFactory
+    {
+        public static WriteableBitmap CreateFromFile(string filePath)
+        {
+            BitmapImage bmpImage = new BitmapImage();
+            WriteableBitmap wBitmap = null;
+            try
+            {
+                using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    bmpImage.BeginInit();
+                    bmpImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bmpImage.StreamSource = fs;
+                    bmpImage.EndInit();
+                }
+
+                wBitmap = new WriteableBitmap(bmpImage);
+            }
+            catch
+            {
+                MessageBox.Show("Файл имеет не верный формат\nили поврежден.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                bmpImage.StreamSource = null;
+                bmpImage = null;
+            }
+            return wBitmap;
+        }
+    }
+}
