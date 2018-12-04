98e09ed586b7f43e1df69c5e71de6d34efed32b9
ArtemChalov <chalov81@mail.ru>
2018-12-05 00:02:28 +0500

    Added BitmapImageFactory class.

---
+++ WriteableBitmapEx/BitmapImageFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteableBitmapEx
+{
+    /// <summary>
+    /// Exposes static methods for create an instance of the BitmapImage class.
+    /// </summary>
+    public static class BitmapImageFactory
+    {
+
+    }
+}
