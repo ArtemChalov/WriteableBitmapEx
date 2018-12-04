8794dc305f380ac841d9bb43b005fa42d141c2b3
ArtemChalov <chalov81@mail.ru>
2018-12-05 00:33:42 +0500

    Implemented CreateFromFileAsync method in the BitmapImageFactory class.

--- WriteableBitmapEx/BitmapImageFactory.cs
+++ WriteableBitmapEx/BitmapImageFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
@@ -32,5 +33,31 @@ namespace WriteableBitmapEx
             return bitmap;
         }
 
+        /// <summary>
+        /// Create a new instance of the BitmapImage class asynchronously.
+        /// </summary>
+        /// <param name="filePath">Full path of the file.</param>
+        /// <returns>The instance of the BitmapImage class</returns>
+        public static Task<BitmapImage> CreateFromFileAsync(string filePath)
+        {
+            BitmapImage bitmap = null;
+            return Task.Factory.StartNew(() => {
+                try
+                {
+                    bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.UriSource = new Uri(filePath);
+                    bitmap.EndInit();
+                    bitmap.Freeze();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Файл имеет не верный формат\nили поврежден.", "Ошибка открытия файла", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                return bitmap;
+            });
+        }
     }
 }
