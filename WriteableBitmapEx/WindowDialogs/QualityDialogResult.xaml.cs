e6efc9b14779910220b14f1720945ccade740481
ArtemChalov <chalov81@mail.ru>
2018-12-05 09:13:56 +0500

    Added QualityDialogResult window.

---
+++ WriteableBitmapEx/WindowDialogs/QualityDialogResult.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace Dialogs.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для QualityDialogResult.xaml
+    /// </summary>
+    public partial class QualityDialogResult : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the QualityDialogResult class.
+        /// </summary>
+        public QualityDialogResult()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The property that keep a slider value 
+        /// </summary>
+        public double Quality { get { return QSlider.Value; } }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
