929d9b77f8a4db41a21b9978529f7d03da59d4e5
ArtemChalov <chalov81@mail.ru>
2018-12-05 08:48:15 +0500

    Added WriteableBitmapSaveEx class

---
+++ WriteableBitmapEx/WriteableBitmapSaveEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WriteableBitmapEx
+{
+    /// <summary>
+    /// Exposes static methods for save an instance of the WriteableBitmap class to the file.
+    /// </summary>
+    public static class WriteableBitmapSaveEx
+    {
+
+    }
+}
